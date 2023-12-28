ef93533868d93c0426a55f104472ae6a7ec87623
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2023-12-28 04:41:27 -0300

    box-commands

--- scripts.js
+++ scripts.js
@@ -8,3 +8,20 @@ document.querySelector(".fa-close").addEventListener("click", () => {
   document.querySelector(".fa-bars").classList.remove("show");
   document.querySelector(".fa-close").classList.remove("show");
 });
+
+//Script Dropdown
+function funcaoBotao1() {
+  // Lógica para o Botão 1
+}
+
+function funcaoBotao2() {
+  // Lógica para o Botão 2
+}
+
+function funcaoBotao3() {
+  // Lógica para o Botão 3
+}
+
+function funcaoBotao4() {
+  // Lógica para o Botão 4
+}
