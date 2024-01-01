22371f67806717b491016763f924808c7ae96856
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-01-01 18:16:16 -0300

    modal inputs

--- modals.js
+++ modals.js
@@ -3,21 +3,32 @@ const closeModalRecebimentos = document.querySelector('.fechar-recebimentos');
 const modalRecebimentos = document.querySelector('.modal-recebimentos');
 
 openModalRecebimentos.addEventListener('click', () => {
-    modalRecebimentos.showModal();
-    document.body.style.overflow = 'hidden';
+    modalRecebimentos.showModal(); // Abre o modal
+    document.body.style.overflow = 'hidden'; // Esconde a barra de rolagem
 });
 
 closeModalRecebimentos.addEventListener('click', () => {
-    closeModal();
+    closeModal(); // Fecha o modal
+    document.querySelector(".modal-form").reset(); // Limpa os campos do formulário
 });
 
 modalRecebimentos.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
-        closeModal();
+        closeModal(); // Fecha o modal com ESC
+        document.querySelector(".modal-form").reset(); // Limpa os campos do formulário
     }
 });
 
 function closeModal() {
     modalRecebimentos.close();
-    document.body.style.overflow = '';
+    document.body.style.overflow = ''; // Faz a barra de rolagem reaparecer
 }
+
+function formatarCampoValor(input) {
+    var valor = input.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+
+    // Verifica se o resultado é NaN
+    valor = isNaN(parseFloat(valor)) ? "0,00" : (parseFloat(valor) / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    input.value = valor;
+  }
