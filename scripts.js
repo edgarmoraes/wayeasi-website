2f5d947767730d9e4f0034bc6648acc1cf7522bc
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-01-04 00:22:22 -0300

    checkbox bar

--- scripts.js
+++ scripts.js
@@ -1,11 +1,144 @@
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector(".nav-options").classList.add("display");
-  document.querySelector(".fa-bars").classList.add("show");
-  document.querySelector(".fa-close").classList.add("show");
+document.addEventListener('DOMContentLoaded', function () {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const botoesAcoes = document.querySelector('.botoes-acoes');
+    const tabelaLancamentos = document.querySelector('.conteudo-tabela-lancamentos');
+    const cancelarButton = document.querySelector('.cancelar-button');
+
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener('change', function () {
+            const algumaCheckboxMarcada = Array.from(checkboxes).some(checkbox => checkbox.checked);
+
+            if (algumaCheckboxMarcada) {
+                // Pelo menos uma checkbox marcada, exibir os botões e adicionar a margem
+                botoesAcoes.style.display = 'flex';
+                tabelaLancamentos.style.marginBottom = '5rem';
+            } else {
+                // Nenhuma checkbox marcada, ocultar os botões e remover a margem
+                botoesAcoes.style.display = 'none';
+                tabelaLancamentos.style.marginBottom = '0';
+            }
+        });
+    });
+
+    cancelarButton.addEventListener('click', function () {
+        checkboxes.forEach(function (checkbox) {
+            checkbox.checked = false;
+        });
+
+        // Ocultar os botões e remover a margem
+        botoesAcoes.style.display = 'none';
+        tabelaLancamentos.style.marginBottom = '0';
+    });
 });
 
-document.querySelector(".fa-close").addEventListener("click", () => {
-  document.querySelector(".nav-options").classList.remove("display");
-  document.querySelector(".fa-bars").classList.remove("show");
-  document.querySelector(".fa-close").classList.remove("show");
+function liquidar() {
+    alert('Ação de liquidar');
+}
+
+function apagar() {
+    alert('Ação de apagar');
+}
+
+
+
+function abrirModal(openBtn, modal, formSelector) {
+    openBtn.addEventListener('click', () => {
+        modalAberto = modal;
+        modal.showModal();
+        document.body.style.overflow = 'hidden';
+    });
+}
+
+function fecharModal(closeBtn, modal, formSelector) {
+    closeBtn.addEventListener('click', () => {
+        fechar(modal, formSelector);
+    });
+
+    modal.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            fechar(modal, formSelector);
+        }
+    });
+}
+
+function fechar(modal, formSelector) {
+    modal.close();
+    document.body.style.overflow = '';
+    document.querySelector(formSelector).reset();
+}
+
+// Elementos do DOM
+const openModalRecebimentos = document.querySelector('.recebimentos');
+const openModalPagamentos = document.querySelector('.pagamentos');
+const openModalTransferencias = document.querySelector('.transferencias');
+
+const closeModalRecebimentos = document.querySelector('.modal-fechar-recebimentos');
+const closeModalPagamentos = document.querySelector('.modal-fechar-pagamentos');
+const closeModalTransferencias = document.querySelector('.modal-fechar-transferencias');
+
+const modalRecebimentos = document.querySelector('.modal-recebimentos');
+const modalPagamentos = document.querySelector('.modal-pagamentos');
+const modalTransferencias = document.querySelector('.modal-transferencias');
+
+// Event Listeners
+abrirModal(openModalRecebimentos, modalRecebimentos, ".modal-form-recebimentos");
+fecharModal(closeModalRecebimentos, modalRecebimentos, ".modal-form-recebimentos");
+
+abrirModal(openModalPagamentos, modalPagamentos, ".modal-form-pagamentos");
+fecharModal(closeModalPagamentos, modalPagamentos, ".modal-form-pagamentos");
+
+abrirModal(openModalTransferencias, modalTransferencias, ".modal-form-transferencias");
+fecharModal(closeModalTransferencias, modalTransferencias, ".modal-form-transferencias");
+
+
+// Função para formatar o valor de um campo como moeda brasileira
+function formatarCampoValor(input) {
+    // Obtém apenas os dígitos do valor atual
+    var valorNumerico = input.value.replace(/\D/g, '');
+
+    // Converte o valor para número
+    var valorDecimal = parseFloat(valorNumerico) / 100;
+
+    // Formata o valor como moeda brasileira
+    var valorFormatado = valorDecimal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Adiciona o "R$" ao valor formatado e atualiza o campo
+    input.value = "R$ " + (isNaN(valorDecimal) ? "0,00" : valorFormatado.replace('R$', '').trim());
+}
+
+// Adiciona um evento de teclado para detectar 'Shift + D'
+document.addEventListener('keydown', function(event) {
+    if (event.shiftKey && event.key === 'D') {
+        event.preventDefault(); // Evita a inserção da tecla no campo
+        preencherDataEFocus();
+    }
 });
+
+// Função para preencher os campos de data e mover o foco para o campo de descrição
+function preencherDataEFocus() {
+    var dataRecebimentos = document.getElementById("data-recebimentos");
+    var dataPagamentos = document.getElementById("data-pagamentos");
+    var dataTransferencias = document.getElementById("data-transferencias");
+
+    // Obter a data atual
+    var dataAtual = new Date();
+    var dia = ('0' + dataAtual.getDate()).slice(-2);
+    var mes = ('0' + (dataAtual.getMonth() + 1)).slice(-2);
+    var ano = dataAtual.getFullYear();
+
+    // Formatando como "yyyy-mm-dd" para ser aceito pelo campo de data
+    var dataFormatada = ano + '-' + mes + '-' + dia;
+
+    // Preenche os campos de data apenas no modal que está aberto
+    if (modalAberto === modalRecebimentos) {
+        dataRecebimentos.value = dataFormatada;
+        document.getElementById('descricao-recebimentos').focus();
+    } else if (modalAberto === modalPagamentos) {
+        dataPagamentos.value = dataFormatada;
+        document.getElementById('descricao-pagamentos').focus();
+    } else if (modalAberto === modalTransferencias) {
+        dataTransferencias.value = dataFormatada;
+        document.getElementById('valor-transferencias').focus();
+    }
+}
+
